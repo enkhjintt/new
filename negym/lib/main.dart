bee0f2b7f1597088765985230c67ff7380dbc0e9
aariuna <aarina507@gmail.com>
2024-05-10 13:33:12 +0800

    Zaya oorchlov

--- negym/lib/main.dart
+++ negym/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'F.IT333 BD2'),
     );
   }
 }
@@ -35,14 +35,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -53,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Management assignment- Github---',
+              'Management assignment- Github---Changed',
             ),
             Text(
               '$_counter',
